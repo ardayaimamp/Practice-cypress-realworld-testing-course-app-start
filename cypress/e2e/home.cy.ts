77cb3f595ff74b942e0a617cca57b08f2e0d00b0
Ardaya Pratama <ardayaimamp@gmail.com>
2022-09-11 00:19:41 +0700

    update test mutiple pages, test user journey

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -3,20 +3,41 @@ describe("home page", () => {
     cy.visit("http://localhost:3000")
   })
   
-  it("the h1 contains the correct text", () => {
-    cy.visit('http://localhost:3000/')
-    // cy.pause();
-    cy.getByData("hero-heading").contains("Testing Next.js Applications with Cypress")
-    
-  })
-  
-  it('the future on the homepage are correct', () => {
+  context("Hero section", () => {
+    it("the h1 contains the correct text", () => {
       cy.visit('http://localhost:3000/')
-      cy.get('dt').eq(0).contains('4 Courses')
-      cy.get('dt').eq(1).contains('25+ Lessons')
-      cy.get('dt').eq(2).contains('Free and Open Source')
+      // cy.pause();
+      cy.getByData("hero-heading").contains("Testing Next.js Applications with Cypress")
       
+    })
+    
+    it('the future on the homepage are correct', () => {
+        cy.visit('http://localhost:3000/')
+        cy.get('dt').eq(0).contains('4 Courses')
+        cy.get('dt').eq(1).contains('25+ Lessons')
+        cy.get('dt').eq(2).contains('Free and Open Source')
+        
+    })
+
   })
 
+  context("Courses section", () => {
+    it("Course: Testing Your First Next.js Application", () => {
+      cy.getByData("course-0").find("a").eq(3).click()
+      cy.location("pathname").should("eq", "/testing-your-first-application")
+
+    })
+
+    it("Course: Testing Foundations", () => {
+      cy.getByData("course-1").find("a").eq(3).click()
+      cy.location("pathname").should("eq", "/testing-foundations")
+    })
+  
+    it("Course: Cypress Fundamentals", () => {
+      cy.getByData("course-2").find("a").eq(3).click()
+      cy.location("pathname").should("eq", "/cypress-fundamentals")
+    })
+
+  })
 
 })
